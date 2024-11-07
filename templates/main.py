9339765b999df822e649ea46a3313e35deb68c9e
Schopp <philipp.schopp@iav.de>
2024-11-07 15:38:06 +0100

    initial

---
+++ templates/main.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import re
+
+def do_main():
+    with open(Path('input.txt')) as file:
+        lines = [line.rstrip() for line in file]
+
+    point_sum = 0
+
+    for line_index, line in enumerate(lines):
+        return
+
+if __name__ == '__main__':
+    do_main()
