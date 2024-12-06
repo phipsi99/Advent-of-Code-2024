ea622e587e60f4bc5ad9ca57e19e7ce0ad91b482
Philipp Schopp <philipp.schopp@iav.de>
2024-12-06 07:29:16 +0100

    5 and  6

--- 05/main.py
+++ 05/main.py
@@ -1,17 +1,87 @@
 from pathlib import Path
 
+
 def do_main(debug_mode=False):
-    with open(Path('05/input.txt')) as file:
+    l = {}
+    updates = []
+    with open(Path("05/input.txt")) as file:
         lines = [line.rstrip() for line in file]
-    
+
     if debug_mode:
-        with open(Path('05/test.txt')) as file:
+        with open(Path("05/test.txt")) as file:
             lines = [line.rstrip() for line in file]
 
     point_sum = 0
 
-    for line_index, line in enumerate(lines):
-        r = [int(i) for i in line.split(" ")]
+    corr_ordered = []
+    incorr_ordered = []
 
-if __name__ == '__main__':
+    update_index = 0
+    for line_index, line in enumerate(lines):
+        if line.strip() == "":
+            update_index = line_index + 1
+            break
+        ll, rr = [int(i) for i in line.split("|")]
+        if ll not in l:
+            l[ll] = []
+        l[ll].append(rr)
+
+    for line in lines[update_index:]:
+        updates.append([int(i) for i in line.split(",")])
+
+    for update in updates:
+        all_ordered = True
+        for index, page in enumerate(update):
+            if page not in l:
+                continue
+            for prev_page in update[:index]:
+                if prev_page in l[page]:
+                    all_ordered = False
+                    break
+            update_w = update[index + 1 :]
+            for next_page in update_w:
+                if next_page not in l[page]:
+                    all_ordered = False
+                    break
+            if not all_ordered:
+                break
+        if all_ordered:
+            corr_ordered.append(update)
+        else:
+            incorr_ordered.append(update)
+
+    for corr in corr_ordered:
+        mid_index = len(corr) // 2
+        point_sum += corr[mid_index]
+    print(point_sum)
+    point_sum = 0
+
+    new_l = {}
+    for rule, pages in l.items():
+        if rule not in new_l:
+            new_l[rule] = []
+        new_l[rule].extend(pages)
+        new_l[rule] = list(set(new_l[rule]))
+
+    incorr_sorted = []
+    for update in incorr_ordered:
+        corrected_update = update[:]
+        for index, page in enumerate(update):
+            if page not in l:
+                continue
+            for index_prev, prev_page in enumerate(corrected_update[:index]):
+                if prev_page in new_l[page]:
+                    corrected_update.remove(page)
+                    corrected_update.insert(index_prev, page)
+                    break
+        incorr_sorted.append(corrected_update)
+
+    for corrected_update in incorr_sorted:
+        mid_index = len(corrected_update) // 2
+        point_sum += corrected_update[mid_index]
+
+    print(point_sum)
+
+
+if __name__ == "__main__":
     do_main(False)
--- 06/main.py
+++ 06/main.py
@@ -1,4 +1,40 @@
+import copy
 from pathlib import Path
+
+import tqdm
+
+def simulate_moves(lines):
+    arr_size = len(lines[0]) * len(lines)
+    directions = [(0,-1), (1,0),(0,1),(-1,0)]
+    sel_dir = 0
+    is_inf = False
+    
+    x = 0
+    y = 0
+    for line_index, line in enumerate(lines):
+        for  char_index, char in enumerate(line):
+            if char == '^':
+                x = char_index
+                y = line_index
+    i = 0
+    while 0 <= x < len(lines[0]) and 0 <= y < len(lines):
+        i += 1
+        if i >= arr_size:
+            is_inf  = True
+            break
+        lines[y][x] = 'X'
+        new_x = x + directions[sel_dir][0]
+        new_y = y + directions[sel_dir][1]
+        if 0 <= new_x < len(lines[0]) and new_y < len(lines):
+            c = lines[new_y][new_x]
+            if c == '#':
+                sel_dir = (sel_dir + 1) % 4
+            else:
+                x = new_x
+                y = new_y
+        else:
+            break
+    return lines, is_inf
 
 def do_main(debug_mode=False):
     with open(Path('06/input.txt')) as file:
@@ -11,7 +47,31 @@ def do_main(debug_mode=False):
     point_sum = 0
 
     for line_index, line in enumerate(lines):
-        r = [int(i) for i in line.split(" ")]
+        lines[line_index] = list(line)
+    o_lines = copy.deepcopy( lines)
+    
+
+    lines, _ = simulate_moves(lines)
+    for line in lines:
+        for  c in line:
+            if c == 'X':
+                point_sum += 1
+
+    print(point_sum)
+
+    inf_cnt = 0
+    for mod_line_index, line in tqdm.tqdm(enumerate(lines),  total=len(lines)):
+        for  mod_char_index, char in tqdm.tqdm( enumerate(line),  total=len(line)):
+            mod_lines =  copy.deepcopy( o_lines)
+            if mod_lines[mod_line_index][mod_char_index] == '#':
+                continue
+            mod_lines[mod_line_index][mod_char_index] = '#'
+
+            _, is_inf = simulate_moves(mod_lines)
+            if is_inf:
+                inf_cnt += 1
+    print(inf_cnt)
+
 
 if __name__ == '__main__':
     do_main(False)
